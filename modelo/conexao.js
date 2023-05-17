2843bdf08579fe67af42c36733527ba48aa19886
Samuel Bozza <102820398+SamuelBozza@users.noreply.github.com>
2023-05-17 09:51:15 -0300

    att

--- modelo/conexao.js
+++ modelo/conexao.js
@@ -1,7 +1,10 @@
-const banco = require('mongoose');
+const banco = require("mongoose");
 const options = {
-    useUnifiedTopology: true,
-    useNewUrlParser   : true
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
 };
 
-module.exports = banco.connect('mongodb://geovannea28:sEF3gN3hihRE9mJC@cluster0.ykff7s4.mongodb.net', options);
+module.exports = banco.connect(
+  "mongodb://geovannea28:sEF3gN3hihRE9mJC@cluster0.ykff7s4.mongodb.net",
+  options
+);
